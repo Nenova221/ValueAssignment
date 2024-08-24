be3783f6dc6d59b6994a58aba5b0fc8f550ef28b
Nenova221 <kalina.nenova@paisii.info>
2024-08-24 14:35:46 +0300

    First commit

---
+++ ValueAssignment/ValueAssignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueAssignment
+{
+    public class ValueAssignment
+    {
+        static void Main(string[] args)
+        {
+            int n = 35, p = 5, v = 0;
+            n = (v == 0) ? n = n & (~(1 << p)) : n = n | (1 << p);
+            Console.WriteLine(n);
+        }
+    }
+}
